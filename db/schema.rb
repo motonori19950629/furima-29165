2c5f294405b0b4fd880e76025e87898ff0286e97
motonori19950629 <s2.k5.i2.a2.a@ezweb.ne.jp>
2020-09-03 23:15:02 +0900

    復活

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_063807) do
+ActiveRecord::Schema.define(version: 2020_09_03_140624) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
