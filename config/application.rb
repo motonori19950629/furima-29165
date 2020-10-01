4b90da696050dbc5276564c5655b030065b15201
motonori19950629 <s2.k5.i2.a2.a@ezweb.ne.jp>
2020-10-01 12:59:55 +0900

    S3

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+require File.expand_path('../boot', __FILE__)
 Bundler.require(*Rails.groups)
 
 module Furima29165
